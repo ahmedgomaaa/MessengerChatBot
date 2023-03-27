41a53d18f92ce3a927c7f9f855bfcaf51e288bd1
ahmedgomaaa <ahmed.goma1010@gmail.com>
2023-03-27 02:08:37 +0200

    chaning port

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const requset = require('request');
 
 const app = express();
 
-app.set('port', (process.env.port || 8000));
+app.set('port', (process.env.PORT || 8000));
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.get('/', function(req, res) {
     res.send('Hi I am a anima');
